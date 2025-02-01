da00161ccc8a37b29e11ea1a7536cf70b598f1c3
Balasaravanan2000 <balasaravanan5126@gmail.com>
2025-02-01 13:40:03 +0530

    process

---
+++ js/2.js
@@ -0,0 +1,6 @@
+function on(){
+    document.getElementById("bulb").src="/exerciseJS/images/dont-forget-to-turn-the-light-off.webp";
+}
+function off(){
+    document.getElementById("bulb").src="/exerciseJS/images/hand-pulling-chain-to-turn-off-light-bulb-green-background.webp";
+}
---
+++ js/ex.js
@@ -0,0 +1,9 @@
+function js() {
+    document.getElementById("demo").innerHTML = "Hello, JavaScript!";
+}
+function bulb(){
+    document.getElementById("onoff").src="/exerciseJS/images/dont-forget-to-turn-the-light-off.webp";
+}
+function bulboff(){
+    document.getElementById("onoff").src="/exerciseJS/images/hand-pulling-chain-to-turn-off-light-bulb-green-background.webp";
+}
