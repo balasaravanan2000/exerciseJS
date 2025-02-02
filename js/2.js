3d897070663843ac026a59085f3ad49ea7214e28
Balasaravanan2000 <balasaravanan5126@gmail.com>
2025-02-02 13:22:51 +0530

    process

--- js/2.js
+++ js/2.js
@@ -1,6 +1,15 @@
-function on(){
+/*function on(){
     document.getElementById("bulb").src="/exerciseJS/images/dont-forget-to-turn-the-light-off.webp";
 }
 function off(){
     document.getElementById("bulb").src="/exerciseJS/images/hand-pulling-chain-to-turn-off-light-bulb-green-background.webp";
+}*/
+window.onload = function() {
+let r=5;
+let h=8;
+function volume(){
+    return 3.14*r*r*h;
+}
+let z=volume();
+document.getElementById("AC").innerHTML= +z;
 }
