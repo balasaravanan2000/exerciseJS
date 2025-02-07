e0f9f5646bcbff8d6f0ada2717dd2a1ded2bbae5
Balasaravanan2000 <balasaravanan5126@gmail.com>
2025-02-07 13:16:03 +0530

    day5

--- js/2.js
+++ js/2.js
@@ -4,7 +4,9 @@
 function off(){
     document.getElementById("bulb").src="/exerciseJS/images/hand-pulling-chain-to-turn-off-light-bulb-green-background.webp";
 }*/
-window.onload = function() {
+
+
+//functions
 let r=5;
 let h=8;
 function volume(){
@@ -31,6 +33,8 @@ document.getElementById("at").innerHTML= +x;
 document.getElementById("mul").innerHTML= +z;
 }
 
+
+//objects
 const car={
     modelname:"jaguar",
     year:2020,
@@ -56,9 +60,53 @@ const student ={
      } 
     }
      document.getElementById("nest").innerHTML=student.name +"'s maths marks is " + student.marks.maths;
-  }
+  
+function cone(r,h){
+    return (1/3)*3.14*r*r*h;
+}
+document.getElementById("cone").innerHTML=cone(5,6);
+
+
+//object methods
+const myname= { 
+    firstname:"jothi",
+    lastname:"balan",
+    fullname : function() {
+        return this.firstname +" " +this.lastname;
+    }
+   };
+    document.getElementById("obj").innerHTML=myname.fullname();
+
+
+ const school={
+    firstname:"srinivasa",
+    lastname:"vidhyalaya",
+    fullname: function(){
+        return this.firstname + " " + this.lastname;
+    }
+ };
+ document.getElementById("sch").innerHTML=school.fullname();
+
+//object constructor
+function students(sname,classofstudent,mark,result){
+    this.sname=sname;
+    this.class=classofstudent;
+    this.mark=mark;
+    this.result=result;
+}
+const ram=new students("ram",12,450,"pass");
+
+document.getElementById("constructor").innerHTML=ram.sname;
 
 
 
+
+function cars(name,model,year){
+    this.name=name;
+    this.model=model;
+    this.year=year;
+}
+const benz=new cars("benz","I6", 2000 );
+document.getElementById("constr").innerHTML=benz.name;
 
 
